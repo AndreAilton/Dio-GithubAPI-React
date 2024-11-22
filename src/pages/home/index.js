d91f4048648d59cef03a48bbb7329a145aa95a5c
Andre <andre.ailtonf.10@gmail.com>
2024-11-21 21:13:31 -0300

    Responsivo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header Nome="props" />
+      <Header Nome="GitHub APi Fetch" />
       <Styled.Conteudo className="background">
         <Styled.Img
           src={background}
